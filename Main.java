11cfa9df6f2e12933f4d7bdf1db75c7d6053362a
Bets Dmytro <pidgua@gmail.com>
2022-10-10 00:27:46 +0300

    Lab1

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
  C2 = 503 % 2 = 1     | O1 = "-"
  C = C3 = 503 % 3 = 2 | C = 2
@@ -7,20 +9,57 @@
 public class Main {
     public static void main(String[] args) {
         double sum = 0;
-        int C = 2;
+        final int C = 2;
         double i, j;
-        double a = 3, n = 9, b = 5, m = 12;
+        double a, n, b, m;
+        Scanner sc = new Scanner(System.in);
+
+        // Input of a
+        System.out.println("Enter a");
+        if (sc.hasNextDouble()) {
+            a = sc.nextDouble();
+        } else {
+            System.out.println("Incorrect input");
+            return;
+        }
+
+       // Input of n
+        System.out.println("Enter n");
+        if (sc.hasNextDouble()) {
+            n = sc.nextDouble();
+        } else {
+            System.out.println("Incorrect input");
+            return;
+        }
+
+        // Input of b
+        System.out.println("Enter b");
+        if (sc.hasNextDouble()) {
+            b = sc.nextDouble();
+        } else {
+            System.out.println("Incorrect input");
+            return;
+        }
+
+       // Input of m
+        System.out.println("Enter m");
+        if (sc.hasNextDouble()) {
+            m = sc.nextDouble();
+        } else {
+            System.out.println("Incorrect input");
+            return;
+        }
+
 
         if (a <= C) {
             System.out.println("Division by zero");
-        }
-        else {
+        } else {
             for (i = a; i < n; i++) {
                 for (j = b; j < m; j++) {
                     sum += (i + j) / (i - C);
                 }
             }
-            System.out.println(sum);
+            System.out.println("Sum is " + sum);
         }
     }
 }
